3d615cdeba6f41ac2718166c7124a1fc80dca6ff
Ricardo <ricardo.breganti@gmail.com>
2021-08-09 22:22:22 -0300

    Etecia

--- MoNaRoda/app/src/main/java/br/com/etecia/maonaroda/MapsActivity.java
+++ MoNaRoda/app/src/main/java/br/com/etecia/maonaroda/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng etecia = new LatLng(-23.702207,-46.6910978);
+        mMap.addMarker(new MarkerOptions().position(etecia).title("etecia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(etecia));
     }
 }
